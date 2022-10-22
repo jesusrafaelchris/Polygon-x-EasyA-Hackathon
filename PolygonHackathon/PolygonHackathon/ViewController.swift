b8ad09215af82c537e82b374a90f8921501beb10
Christian Grinling <grinlingc@gmail.com>
2022-10-22 12:01:53 +0100

    Added iphone app files

---
+++ PolygonHackathon/PolygonHackathon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PolygonHackathon
+//
+//  Created by Christian Grinling on 22/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
